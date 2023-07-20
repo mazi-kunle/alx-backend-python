aa142c71fa2d79d7dd75154ac7158b45076e9aa5
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-20 20:17:59 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    A function that takes a list of floats and
+    returns their sum as float
+    '''
+    return sum(input_list)
