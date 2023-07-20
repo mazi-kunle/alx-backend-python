48097104b12c2b0751245becb23c74117221a056
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-20 19:53:43 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    A function that takes two strings and returns
+    their concatenation.
+    '''
+    return str1 + str2
