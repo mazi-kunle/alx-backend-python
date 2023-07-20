cd32c4801d43bd8ec8bf25b9095ced44850105c1
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-20 21:12:25 +0100

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    '''
+    A function that takes a string and an int or float
+    and returns a tuple
+    '''
+    return (str, float(v ** 2))
