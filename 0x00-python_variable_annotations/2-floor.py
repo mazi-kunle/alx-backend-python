2c1c11811de82c802d27a821d206767b79449b69
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-20 19:57:26 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+
+def floor(n: float) -> int:
+    '''
+    A function that takes a float and returns the
+    floor of the float
+    '''
+    return int(n)
