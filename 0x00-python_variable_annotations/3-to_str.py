d4a12561240eab1485f88a960a8efcbeab142c1e
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-20 19:59:18 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+
+def to_str(n: float) -> str:
+    '''
+    A function that takes a float and returns
+    the string representation of the float
+    '''
+    return str(n)
