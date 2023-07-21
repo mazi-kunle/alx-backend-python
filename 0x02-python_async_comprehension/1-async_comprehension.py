5f91896c273ff11207ccea0604cb5de7dd1b1de2
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-21 20:36:57 +0100

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''This is a module'''
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''A function'''
+    return [i async for i in async_generator()]
