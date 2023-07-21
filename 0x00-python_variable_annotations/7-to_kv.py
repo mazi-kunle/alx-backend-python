894a66ae4b5e072258546cc74ae9553b672a8b5c
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-21 03:05:51 +0100

    7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     A function that takes a string and an int or float
     and returns a tuple
