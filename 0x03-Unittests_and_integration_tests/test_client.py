8fea285abe3b8819039d5acde78ff33dc88a98af
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-12 02:05:05 +0100

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,7 +40,7 @@ class TestGithubOrgClient(unittest.TestCase):
         a test method
         '''
         test = GithubOrgClient('google')
-        
+
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock) as mock_org:
             mock_org.return_value = {
@@ -50,3 +50,50 @@ class TestGithubOrgClient(unittest.TestCase):
                 test._public_repos_url,
                 "https://api.github.com/users/google/repos"
             )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_getJson):
+        '''
+        a test method
+        '''
+        payload = [{
+            "login": "google",
+            "id": 1342004,
+            "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
+            "url": "https://api.github.com/orgs/google",
+            "repos_url": "https://api.github.com/orgs/google/repos",
+            "events_url": "https://api.github.com/orgs/google/events",
+            "hooks_url": "https://api.github.com/orgs/google/hooks",
+            "issues_url": "https://api.github.com/orgs/google/issues",
+            "members_url": "https://api.github.com/orgs/google/members",
+            "public_members_url": "https://api.github.com/orgs/google",
+            "avatar_url": "https://avatars.githubusercontent.com/u",
+            "description": "Google ❤️ Open Source",
+            "name": "Google",
+            "company": None,
+            "blog": "https://opensource.google/",
+            "location": None,
+            "email": "opensource@google.com",
+            "twitter_username": "GoogleOSS",
+            "is_verified": True,
+            "has_organization_projects": True,
+            "has_repository_projects": True,
+            "public_repos": 2531,
+            "public_gists": 0,
+            "followers": 27361,
+            "following": 0,
+            "html_url": "https://github.com/google",
+            "created_at": "2012-01-18T01:30:18Z",
+            "updated_at": "2021-12-30T01:40:20Z",
+            "archived_at": None,
+            "type": "Organization"
+        }]
+        mock_getJson.return_value = payload
+        test = GithubOrgClient('google')
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock:
+            mock.return_value = payload[0]['repos_url']
+            self.assertEqual(test.public_repos(), ['Google'])
+            mock_getJson.assert_called_once()
+            mock.assert_called_once()
